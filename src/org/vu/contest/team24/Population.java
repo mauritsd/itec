ae70a3d2ce41760de3852df91dc6e5142a96f595
RobbinBouwm <r2.bouwmeester@student.vu.nl>
2013-09-28 15:53:52 +0200

    Added tournament selection.

--- src/org/vu/contest/team24/Population.java
+++ src/org/vu/contest/team24/Population.java
@@ -10,11 +10,15 @@ package org.vu.contest.team24;
  * @author asus
  */
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Population {
     
        SphereEvaluation evaluation2_ = new SphereEvaluation();
+       Random randomGenerator = new Random();
        
        public double[] initiateMember(int genomeSize){
             double[] genome = new double[genomeSize];
@@ -40,7 +44,30 @@ public class Population {
             double score = Double.valueOf(evaluation2_.evaluate(member).toString());
             return score;}
         
-        public selectParentsMethodx(){
+        public double[][][] parentSelectionTournament(int numberOfTournaments,int tournamentSize,double[][] orderedParents){
+            double[][][] selectedParents = new double[tournamentSize][][];
+            for (int i=0; i<numberOfTournaments; i++){
+                selectedParents[i] = tournamentBattle(tournamentSize, orderedParents);
+            }
+            return selectedParents;
+        }
+        
+        public double[][] tournamentBattle(int tournamentSize,double[][] orderedParents){
+            int c1 = randomGenerator.nextInt(orderedParents.length);
+            int c2 = randomGenerator.nextInt(orderedParents.length);
+            int c3 = randomGenerator.nextInt(orderedParents.length);
+
+            ArrayList<Integer> selected = new ArrayList<Integer>();
+            selected.add(c1);
+            selected.add(c2);
+            selected.add(c3);
             
+            Collections.sort(selected);
+            double[] parent1 = orderedParents[selected.get(0)];
+            double[] parent2 = orderedParents[selected.get(1)];
+            
+            double[][] parents = {parent1,parent2};
+            
+            return parents;         
         }
 }
